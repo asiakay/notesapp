627e21f2f523a89e5f0ee8eae59471dbfff3c3ee
Asia Grady <66960776+asiakay@users.noreply.github.com>
2021-04-09 23:44:57 -0400

    added comments to describe what the code is doing

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
+// still figuring out why the return is 1
 function getNoteId(){
     let noteObject = getExistingNotes()
     if (!noteObject){
         return 1
     }
+
+    // also still figuring out how to describe this part 
+    // it was explained but I forgot how to explain the way it relates to the rest of the app
     const keysArray = Object.keys(noteObject)
     const numberKeys = keysArray.map((key) => Number(key))
     console.log(numberKeys)
-    
-    return Math.max(...numberKeys) +1
-}
-// get the existing note
 
+    return Math.max(...numberKeys) +1 // this part with the +1 adds a new key to match the new note value 
+}
+// getting the existing notes
+// this function instructs the browser to retrieve the "note" from local storage  
 function getExistingNotes() {
     let notes = localStorage.getItem("note")
     if (!notes){
         return null 
     }
-    return JSON.parse(notes)
+    return JSON.parse(notes) // here we're returning the notes variable as JSON data / not to be confused with the "note" id from the textarea 
     
     
 }
