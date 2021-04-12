b1ccd46ea2864af4df25774aa92a26a39fec0f60
Asia Grady <66960776+asiakay@users.noreply.github.com>
2021-04-12 11:38:21 -0400

    updated comments on getNoteId function & Object.keys method

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-// still figuring out why the return is 1
+// update - The purpose of the getNoteId() is to return the next number in the sequence of notes
+// the return is 1 because it is attached to the if statement that states, it there is no existing noteObject (!noteObject)
+// then the newly created/ first / only noteObject is assigned a value of 1 
 function getNoteId(){
     let noteObject = getExistingNotes()
     if (!noteObject){
         return 1
     }
 
-    // also still figuring out how to describe this part 
+    // big thanks to my code mentor and mozilla docs for helping me understand and describe this part 
     // it was explained but I forgot how to explain the way it relates to the rest of the app
+    // "Object.keys()": method that returns an array of the given parameter, in this case (noteObject). 
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+    // "Object.keys(noteObject)": cycles through the sequence of properties contained /defined within the object. 
+    // In the case of our (noteObject), their are two properties, 1) the number of the note = [id], and the textarea contents = "note"
+    // the reason why Object.keys() method is used on noteObject is because it turns both [id] and "note" properties into string data types
+    // they need to be converted to string dataType to be translated into JSON 
+    // that process of converting the noteObject properties into JSON is stored in "const keysArray = Object.keys(noteObject)
     const keysArray = Object.keys(noteObject)
     const numberKeys = keysArray.map((key) => Number(key))
     console.log(numberKeys)
